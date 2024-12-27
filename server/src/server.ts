bb7ca23694215bda7141bfc6fed5f4b6409c30b4
Nikhil-Sadhwani <137155995+Nikhil-Sadhwani@users.noreply.github.com>
2024-12-27 14:21:55 +0530

    Fixes vercel error

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,13 +21,14 @@ app.post('/api/contact', contactController.handleContact);
 
 // Test route
 app.get('/api/test', (req: Request, res: Response) => {
-  res.json({ message: 'Server is running!' });
+  res.status(200).json({ message: 'Server is running!' });
 });
 
-export default app;
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
 }
+  
+export default app;
